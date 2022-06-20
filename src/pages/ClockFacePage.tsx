0be153d8379cbbddb8184dbd18790fe056843fb5
ckrier <connorkrier@gmail.com>
2022-06-20 14:40:42 -0700

    show next alarm on the clock face

--- src/pages/ClockFacePage.tsx
+++ src/pages/ClockFacePage.tsx
@@ -1,22 +1,127 @@
+import {
+  AlarmOffOutlined,
+  AlarmOnOutlined,
+  AlarmOutlined,
+} from '@mui/icons-material';
+import { Stack } from '@mui/material';
 import { DateTime } from 'luxon';
 import React, { useEffect, useState } from 'react';
+import { getAlarms } from '../api/alarmApi';
 import { stopSound } from '../api/soundApi';
+import { AlarmConfiguration } from '../types/alarmConfiguration';
+import {
+  formatHour,
+  formatMinute,
+  formatTime,
+  getAmPm,
+  getNextScheduledDay,
+  isScheduledTomorrow,
+  isToday,
+  toDateTime,
+} from '../util/timeHelpers';
 
 const ClockFacePage: React.FC = () => {
-  const [currentTime, setCurrentTime] = useState(new Date());
+  const [currentTime, setCurrentTime] = useState(DateTime.now());
+  const [alarms, setAlarms] = useState<AlarmConfiguration[]>([]);
 
   useEffect(() => {
-    const interval = setInterval(() => setCurrentTime(new Date()), 1000);
+    const storeAlarms = async () => {
+      setAlarms(await getAlarms());
+    };
+
+    storeAlarms();
+
+    const updateInterval = setInterval(
+      () => setCurrentTime(DateTime.now()),
+      1000
+    );
+
+    const refreshInterval = setInterval(
+      async () => setAlarms(await getAlarms()),
+      1000 * 60
+    );
 
     return () => {
-      clearInterval(interval);
+      clearInterval(updateInterval);
+      clearInterval(refreshInterval);
     };
   }, []);
+
+  const getNextAlarm = () => {
+    const enabled = alarms.filter((a) => a.enabled);
+
+    if (enabled.length === 0) {
+      return (
+        <>
+          <AlarmOffOutlined color="disabled" fontSize="medium" />
+          <span style={{ fontSize: 20 }}>No upcoming alarms</span>
+        </>
+      );
+    }
+
+    enabled.sort((a, b) => {
+      const dtA = toDateTime(a);
+      const dtB = toDateTime(b);
+
+      return dtA.diff(dtB).minutes;
+    });
+
+    const first = enabled[0];
+    const time = `${formatTime(first.hour, first.minute)} ${getAmPm(
+      first.hour
+    )}`;
+
+    if (isToday(first)) {
+      return (
+        <>
+          <AlarmOnOutlined color="success" fontSize="medium" />
+          <span style={{ fontSize: 20 }}>Today - {time}</span>
+        </>
+      );
+    } else if (first.schedule && isScheduledTomorrow(first.schedule)) {
+      return (
+        <>
+          <AlarmOnOutlined color="success" fontSize="medium" />
+          <span style={{ fontSize: 20 }}>Tomorrow - {time}</span>
+        </>
+      );
+    } else if (first.schedule) {
+      return (
+        <>
+          <AlarmOutlined color="secondary" fontSize="medium" />
+          <span style={{ fontSize: 20 }}>{`${getNextScheduledDay(
+            first.schedule
+          )} - ${time}`}</span>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <AlarmOnOutlined color="success" fontSize="medium" />
+          <span style={{ fontSize: 20 }}>Tomorrow - {time}</span>
+        </>
+      );
+    }
+  };
+
+  const timeStyles = {
+    color: 'white',
+    fontSize: '128px',
+    fontWeight: 700,
+    lineHeight: '128px',
+  };
+
+  const infoStyles = {
+    color: 'white',
+    fontSize: '32px',
+    fontWeight: 600,
+  };
 
   return (
     <div
       style={{
         display: 'flex',
+        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: 'black',
@@ -25,15 +130,25 @@ const ClockFacePage: React.FC = () => {
       }}
       onClick={stopSound}
     >
-      <h1
-        style={{
-          color: 'white',
-          fontSize: '8rem',
-          fontWeight: 700,
-        }}
-      >
-        {DateTime.fromJSDate(currentTime).toLocaleString(DateTime.TIME_SIMPLE)}
-      </h1>
+      <div>
+        <Stack direction="row">
+          <span style={timeStyles}>{formatHour(currentTime.hour)}</span>
+          <span style={timeStyles}>:</span>
+          <span style={timeStyles}>{formatMinute(currentTime.minute)}</span>
+          <Stack padding="8px" justifyContent="space-evenly">
+            <div style={infoStyles}>{getAmPm(currentTime.hour)}</div>
+            <div style={infoStyles}>{currentTime.weekdayShort}</div>
+          </Stack>
+        </Stack>
+        <Stack
+          direction="row"
+          alignItems="center"
+          alignSelf="start"
+          spacing="8px"
+        >
+          {getNextAlarm()}
+        </Stack>
+      </div>
     </div>
   );
 };
--- src/util/timeHelpers.ts
+++ src/util/timeHelpers.ts
@@ -1,3 +1,6 @@
+import { DateTime } from 'luxon';
+import { AlarmConfiguration, DayOfWeek } from '../types/alarmConfiguration';
+
 export const formatMinute = (minute: number): string => {
   return minute < 10 ? `0${minute}` : String(minute);
 };
@@ -12,3 +15,78 @@ export const formatTime = (hour: number, minute: number): string => {
 };
 
 export const getAmPm = (hour: number) => (hour >= 12 ? 'PM' : 'AM');
+
+export const isToday = (al: AlarmConfiguration): boolean => {
+  const dt = DateTime.fromObject({ hour: al.hour, minute: al.minute });
+  const midnight = DateTime.now().plus({ days: 1 }).startOf('day');
+
+  const isTimeToday =
+    dt.diffNow('minutes').minutes > 0 &&
+    dt.diff(midnight, 'minutes').minutes < 0;
+
+  if (al.schedule) {
+    return isScheduledToday(al.schedule) && isTimeToday;
+  }
+
+  return isTimeToday;
+};
+
+export const getNextScheduledDay = (
+  schedule: DayOfWeek[],
+  date: Date = new Date()
+): DayOfWeek => {
+  const dt = DateTime.fromJSDate(date);
+  const lcSched = schedule.map((dow) => dow.toLowerCase());
+
+  for (let dayOffset = 0; dayOffset < 7; ++dayOffset) {
+    const nextDay = dt.plus({ days: dayOffset });
+    if (lcSched.includes(nextDay.weekdayLong.toLowerCase())) {
+      return nextDay.weekdayLong as DayOfWeek;
+    }
+  }
+
+  return dt.weekdayLong as DayOfWeek;
+};
+
+export const getDayNumber = (day: DayOfWeek) => {
+  switch (day) {
+    case 'Monday':
+      return 1;
+    case 'Tuesday':
+      return 2;
+    case 'Wednesday':
+      return 3;
+    case 'Thursday':
+      return 4;
+    case 'Friday':
+      return 5;
+    case 'Saturday':
+      return 6;
+    case 'Sunday':
+      return 7;
+  }
+};
+
+export const isScheduledToday = (schedule: DayOfWeek[]): boolean => {
+  const today = DateTime.now().weekdayLong.toLowerCase();
+  return schedule.map((d) => d.toLowerCase()).includes(today);
+};
+
+export const isScheduledTomorrow = (schedule: DayOfWeek[]): boolean => {
+  const tomorrow = DateTime.now().plus({ days: 1 }).weekdayLong.toLowerCase();
+  return schedule.map((d) => d.toLowerCase()).includes(tomorrow);
+};
+
+export const toDateTime = (al: AlarmConfiguration): DateTime => {
+  let dt = DateTime.fromObject({ hour: al.hour, minute: al.minute });
+
+  if (al.schedule) {
+    dt = dt.set({ weekday: getDayNumber(getNextScheduledDay(al.schedule)) });
+  }
+
+  if (dt.diffNow().minutes < 0) {
+    dt = dt.plus({ days: 1 });
+  }
+
+  return dt;
+};
